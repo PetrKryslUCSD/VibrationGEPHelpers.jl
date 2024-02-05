30ea8220291e00e4f9b1460730afaae281600f62
Petr Krysl <pkrysl@ucsd.edu>
2024-02-05 09:55:06 -0800

    return separately maximum indexes

--- src/check.jl
+++ src/check.jl
@@ -5,22 +5,23 @@ Check the mass-orthogonality of the eigenvectors.
 
 # Returns
 
-- `max_vMv_diag_error`, `max_vMv_offdiag_error`, `max_i`, `max_j`: absolute deviations of the
-  diagonal entries of the reduced mass matrix from unity, absolute deviations of
-  the off-diagonal entries of the reduced mass matrix from zero, and the row and
-  column index of the entry with the largest error.
+- `max_vMv_diag_error`, `max_vMv_offdiag_error`, `max_diag_ij`, `max_i`,
+  `max_j`: absolute deviations of the diagonal entries of the reduced mass
+  matrix from unity, absolute deviations of the off-diagonal entries of the
+  reduced mass matrix from zero, and the row/column diagonal index, the row and
+  column index of the off-diagonal entry with the largest error.
 """
 function check_M_orthogonality(v, M)
     max_vMv_diag_error = 0.0
     max_vMv_offdiag_error = 0.0
-    max_i, max_j = 0, 0
+    max_diag_ij, max_i, max_j = 0, 0, 0
     Mred = v' * M * v
     for i in axes(Mred, 1), j = i:size(Mred, 2)
         p = (Mred[i, j] + Mred[j, i]) / 2
         if i == j
             if abs(p - 1) > max_vMv_diag_error
                 max_vMv_diag_error = abs(p - 1)
-                max_i, max_j = i, j
+                max_diag_ij = i
             end
         else
             if abs(p) > max_vMv_offdiag_error
@@ -29,7 +30,7 @@ function check_M_orthogonality(v, M)
             end
         end
     end
-    return max_vMv_diag_error, max_vMv_offdiag_error, max_i, max_j
+    return max_vMv_diag_error, max_vMv_offdiag_error, max_diag_ij, max_i, max_j
 end
 
 """
@@ -39,23 +40,25 @@ Check the stiffness-orthogonality of the eigenvectors.
 
 # Returns
 
-- `max_vKv_diag_error`, `max_vKv_offdiag_error`, `max_i`, `max_j`: absolute deviations of the
-  diagonal entries of the reduced stiffness matrix from the eigenvalue squared,
-  absolute deviations of the off-diagonal entries of the reduced stiffness
-  matrix from zero, and the row and column index of the entry with the largest error.
+- `max_vKv_diag_error`, `max_vKv_offdiag_error`, `max_diag_ij`, `max_i`,
+  `max_j`: absolute deviations of the diagonal entries of the reduced stiffness
+  matrix from the eigenvalue squared, absolute deviations of the off-diagonal
+  entries of the reduced mass matrix from zero, and the row/column diagonal
+  index, the row and column index of the off-diagonal entry with the largest
+  error.
 
 """
 function check_K_orthogonality(d, v, K)
     max_vKv_diag_error = 0.0
     max_vKv_offdiag_error = 0.0
-    max_i, max_j = 0, 0
+    max_diag_ij, max_i, max_j = 0, 0, 0
     Kred = v' * K * v
     for i in eachindex(d), j = i:length(d)
         p = (Kred[i, j] + Kred[j, i]) / 2
         if i == j
             if abs(p - d[i]) > max_vKv_diag_error
                 max_vKv_diag_error = abs(p - d[i])
-                max_i, max_j = i, j
+                max_diag_ij = i
             end
         else
             if abs(p) > max_vKv_offdiag_error
@@ -64,5 +67,5 @@ function check_K_orthogonality(d, v, K)
             end
         end
     end
-    return max_vKv_diag_error, max_vKv_offdiag_error, max_i, max_j
+    return max_vKv_diag_error, max_vKv_offdiag_error, max_diag_ij, max_i, max_j
 end
